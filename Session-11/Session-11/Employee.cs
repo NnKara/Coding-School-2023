a9495a7d1f5040eba679e64799f5fd61921cf4fc
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-23 16:17:55 +0200

    Employee Created - vlasis

---
+++ Session-11/Session-11/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_11
+{
+    public class Employee : Person
+    {
+        enum EmployeeTypeEnum  
+        {
+        Manager,
+        Staff
+        }
+        public double Salary { get; set; }
+        EmployeeTypeEnum EmployeeType { get; set; }
+
+        public Employee() { 
+        
+        }
+    }
+
+}
