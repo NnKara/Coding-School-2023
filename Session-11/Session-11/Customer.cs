235c82c9b5f575d7e6c54cd247f3c3396c3236ee
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-23 14:44:29 +0200

    Pet Created - Vlasis

--- Session-11/Session-11/Customer.cs
+++ Session-11/Session-11/Customer.cs
@@ -8,14 +8,14 @@ namespace Session_11
 {
     public class Customer
     {
-        public Guid CustormerID { get; set; }
+        public Guid CustomerID { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Phone { get; set; }
         public string TIN { get; set; }
 
-        public Customer() { 
-
+        public Customer() {
+            CustomerID = Guid.NewGuid();
         }
 
     }
---
+++ Session-11/Session-11/Pet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_11
+{
+    public class Pet
+    {
+        enum AnimalTypeEnum
+        {
+            Bird,
+            Cat,
+            Dog
+        }
+        enum PetStatusEnum
+        {
+            OK,
+            Unhealthy,
+            Recovering
+        }
+        public Guid PetID { get; set; }
+        public string Breed { get; set; }
+        AnimalTypeEnum AnimalType { get; set; }
+        PetStatusEnum Status { get; set; }
+        public int Price { get; set; }
+        public int Cost { get; set; }
+
+        public Pet(){
+            PetID = Guid.NewGuid();           
+        }
+
+
+    }
+}
