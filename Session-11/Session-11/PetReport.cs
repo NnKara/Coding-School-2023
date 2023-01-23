e61af198defaf289e04ec53fba68d7e7cdd0e0f8
GaDAlex <alexaras1311@gmail.com>
2023-01-23 16:14:47 +0300

    PetReport change

--- Session-11/Session-11/PetReport.cs
+++ Session-11/Session-11/PetReport.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Session_11.Pet;
 
 namespace Session_11 {
     public class PetReport {
         public DateOnly Year { get; set; }
         public DateOnly Month { get; set; }
-        public Enum AnimalType { get; set; }
+        public AnimalTypeEnum AnimalType { get; set; }
         public double totalSold { get; set; }
 
         public PetReport() {
