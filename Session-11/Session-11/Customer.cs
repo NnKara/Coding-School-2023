901a7873936cdd87af6ad39a5d831b95b9c2164b
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-23 14:33:24 +0200

    Customer Created

---
+++ Session-11/Session-11/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_11
+{
+    public class Customer
+    {
+        public Guid CustormerID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string TIN { get; set; }
+
+        public Customer() { 
+
+        }
+
+    }
+}
