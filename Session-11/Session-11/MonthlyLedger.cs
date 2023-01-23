ce127064559eac05d6e2e5d15c05f12f3aafff79
NnKara <113102125+NnKara@users.noreply.github.com>
2023-01-23 15:38:25 +0200

    Transaction-Monthly Ledger classes upload- Nikos

---
+++ Session-11/Session-11/MonthlyLedger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Session_11
+{
+    public class MonthlyLedger
+    {
+        public DateOnly Year { get; set; }
+        public DateOnly Month { get; set; }
+
+        public double Income { get; set; }
+        public double Expenses { get; set; }
+        public double Total { get; set; }
+    }
+}
---
+++ Session-11/Session-11/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_11
+{
+    internal class Transaction
+    {
+        public Guid TransactionID { get; set; }
+        public DateOnly TransactionDate { get; set; }
+        public Guid CustomerID { get; set; }
+        public Guid EmployeeID { get; set; }
+        public Guid PetID { get; set; } 
+        public double PetPrice { get; set; }
+        public Guid PetFoodID { get; set; }
+        public double PetFoodQty { get; set; }
+        public double PetFoodPrice { get; set; }
+        public double TotalPrice { get; set; }  
+    }
+}
