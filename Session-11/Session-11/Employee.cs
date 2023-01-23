d31a7bc5ffde3f3db651a0a59b31e11d544fb5bf
GaDAlex <alexaras1311@gmail.com>
2023-01-23 16:24:37 +0300

    fix employee constructor

--- Session-11/Session-11/Employee.cs
+++ Session-11/Session-11/Employee.cs
@@ -4,20 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Session_11
-{
-    public class Employee : Person
-    {
-        enum EmployeeTypeEnum  
-        {
-        Manager,
-        Staff
+namespace Session_11 {
+    public class Employee : Person {
+        enum EmployeeTypeEnum {
+            Manager,
+            Staff
         }
+        public Guid EmployeeID { get; set; }
         public double SalaryPerMonth { get; set; }
         EmployeeTypeEnum EmployeeType { get; set; }
 
-        public Employee() { 
-        
+        public Employee(Guid id) : base(id) {
+            EmployeeID = id;
         }
     }
 
--- Session-11/Session-11/Transaction.cs
+++ Session-11/Session-11/Transaction.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Session_11 {
     internal class Transaction {
         public Guid TransactionID { get; set; }
-        public DateOnly TransactionDate { get; set; }
+        public DateTime TransactionDate { get; set; }
         public Guid CustomerID { get; set; }
         public Guid EmployeeID { get; set; }
         public Guid PetID { get; set; }
@@ -18,7 +18,7 @@ namespace Session_11 {
         public double PetFoodPrice { get; set; }
         public double TotalPrice { get; set; }
 
-        public Transaction(DateOnly transactionDate, Guid customerID, Guid employeeID, Guid petID, double petPrice, Guid petFoodID, double petFoodQty, double petFoodPrice, double totalPrice) {
+        public Transaction(DateTime transactionDate, Guid customerID, Guid employeeID, Guid petID, double petPrice, Guid petFoodID, double petFoodQty, double petFoodPrice, double totalPrice) {
             TransactionID = new Guid();
             TransactionDate = transactionDate;
             CustomerID = customerID;
