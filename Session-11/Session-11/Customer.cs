b0446c291d90f3eb88fc5a83d66edaaad9834982
GaDAlex <alexaras1311@gmail.com>
2023-01-23 16:05:33 +0300

    constructor change 2

--- Session-11/Session-11/Customer.cs
+++ Session-11/Session-11/Customer.cs
@@ -4,19 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Session_11
-{
-    public class Customer : Person
-    {
+namespace Session_11 {
+    public class Customer : Person {
         public Guid CustomerID { get; set; }
         public string Phone { get; set; }
         public string TIN { get; set; }
 
-        public Customer(Guid id):base (id) {
+        public Customer(Guid id) : base(id) {
             CustomerID = id;
-
-
-            
         }
 
     }
