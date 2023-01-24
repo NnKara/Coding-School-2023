8e862c4f36eb2454f88fe577f14fc86e1554188c
vlasis votsos <vlasisvotsos@gmail.com>
2023-01-24 15:06:12 +0200

    fix - vlasis

--- Session-11/Session-11/Stock.cs
+++ Session-11/Session-11/Stock.cs
@@ -27,27 +27,31 @@ namespace Session_11 {
             return PetFoodQty;
         }
 
-            return PetsNumber;
-        }
-
-        public double AddPetFoodToStock(double petFoodQuantity) {
+             public double AddPetFoodToStock(double petFoodQuantity)
+        {
             PetFoodQty = GetPetFoodStock() + petFoodQuantity;
             return PetFoodQty;
         }
-        public double RemovePetFoodFromStock(double petFoodQuantity) {
+        public double RemovePetFoodFromStock(double petFoodQuantity)
+        {
             PetFoodQty = GetPetFoodStock() - petFoodQuantity;
             return PetFoodQty;
         }
 
-        public int AddPetsToStock(int petNumber) {
-            PetsNumber = GetPetsNumberStock() + petNumber;  
-            return PetsNumber;  
+        public int AddPetsToStock(int petNumber)
+        {
+            PetsNumber = GetPetsNumberStock() + petNumber;
+            return PetsNumber;
         }
 
-        public int RemovePetsFromStock(int petNumber) {
+        public int RemovePetsFromStock(int petNumber)
+        {
             PetsNumber = GetPetsNumberStock() - petNumber;
             return PetsNumber;
         }
     }
-}
+
+       
+    }
+
 
