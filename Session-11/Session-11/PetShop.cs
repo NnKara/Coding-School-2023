8eadd72d90f3d10014ae406ebd733354d948920c
Antigoni Kasioura <antigoni.k@hotmail.com>
2023-01-24 13:27:54 +0200

    class PetShop

---
+++ Session-11/Session-11/PetShop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_11
+{
+    public class PetShop
+    {
+        public List<Pet> pets = new List<Pet>(); 
+        public List<Customer> customers = new List<Customer>();
+        public List<Employee> employees = new List<Employee>();
+        public List<PetFood> petfoods = new List<PetFood>();
+        public List<Transaction> transactions = new List<Transaction>();
+
+
+    }
+}
