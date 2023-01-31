9ab2ddb7e1bff6c88dc0f1ce13afd5ae8f9dc13a
NnKara <113102125+NnKara@users.noreply.github.com>
2023-01-31 15:58:00 +0200

    Add Crud Interface And Implement Customer.

---
+++ Session-16/Orm/Repository/CustomerRepo.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using Orm.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orm.Repository
+{
+    public class CustomerRepo : EntityInterface<Customer>
+    {
+        public void Add(Customer entity)
+        {
+            using var petShopContext = new PetShopContext();
+            petShopContext.Add(entity);
+            petShopContext.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            using var petShopContext=new PetShopContext();
+            var dbCustomer = petShopContext.Customers.Where(customer => customer.CustomerID == id).SingleOrDefault();
+            if(dbCustomer is null)           
+                return;
+            petShopContext.Remove(dbCustomer);
+            petShopContext.SaveChanges();
+            
+        }
+
+        public IList<Customer> GetAll()
+        {
+            using var petShopContext = new PetShopContext();
+            return petShopContext.Customers.Include(customer => customer.CustomerID)
+                .Include(customer => customer.CustomerName)
+                .Include(customer => customer.CustomerSurname)
+                .Include(customer => customer.Phone)
+                .Include(customer => customer.TIN).ToList();
+                
+        }
+
+        public Customer? GetById(Guid id)
+        {
+            using var petShopContext = new PetShopContext();
+            return petShopContext.Customers.Include(customer => customer.CustomerID)
+               .Include(customer => customer.CustomerName)
+               .Include(customer => customer.CustomerSurname)
+               .Include(customer => customer.Phone)
+               .Include(customer => customer.TIN).SingleOrDefault();
+
+        }
+
+        public void Update(Guid id, Customer entity)
+        {
+            using var petShopContext = new PetShopContext();
+            var dbCustomer=petShopContext.Customers.Where(customer=>customer.CustomerID==id).SingleOrDefault(); 
+            if(dbCustomer is null)
+                return;
+            dbCustomer.CustomerName = entity.CustomerName;
+            dbCustomer.CustomerSurname=entity.CustomerSurname;
+            dbCustomer.Phone= entity.Phone;
+            dbCustomer.TIN=entity.TIN;
+            petShopContext.SaveChanges();
+        }
+    }
+}
