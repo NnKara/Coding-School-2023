cb58547e7bf2e6d14d6e3997b3dc1486a91e9436
NnKara <113102125+NnKara@users.noreply.github.com>
2023-02-08 16:03:59 +0200

    test

---
+++ PetShop/Session-23/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Session_23.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
