b711a46c46e6186aecd4134e54f467a1aea257a2
NnKara <113102125+NnKara@users.noreply.github.com>
2023-02-13 22:01:52 +0200

    testing Blazor

---
+++ Session-23/Session-24/Shared/CustomerListDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_24.Shared {
+
+    public class CustomerListDto {
+
+    public string CustomerName { get; set; }
+    public string CustomerSurname { get; set; }
+    public string CustomerPhone { get; set; }
+
+
+}
+}
